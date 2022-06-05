9150cbd9c867765c3c20be3a5f32df8c2ca7386f
htanzeel01 <54932095+htanzeel01@users.noreply.github.com>
2022-06-05 23:09:45 +0200

    day limit

--- spring-server-generated/src/main/java/io/swagger/repository/TransactionRepository.java
+++ spring-server-generated/src/main/java/io/swagger/repository/TransactionRepository.java
@@ -3,9 +3,12 @@ package io.swagger.repository;
 import io.swagger.model.Account;
 import io.swagger.model.Transactions;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -16,5 +19,11 @@ public interface TransactionRepository extends JpaRepository<Transactions,Intege
     Iterable<Transactions> getAllByAccountto(String iban);
     @Query("SELECT t FROM Transactions AS t WHERE t.accountfrom LIKE ?1 OR t.accountto LIKE ?1 ORDER BY t.transactionDate ASC")
     Iterable<Transactions> getTransactionsByIban(String iban);
+    @Query ("SELECT SUM (amount) from Transactions  AS t where t.accountfrom = ?1 AND t.transactionDate = current_date ")
+    int SumTransaction (String iban);
 
+    @Transactional
+    @Modifying
+    @Query("update Transactions set DayLimit = ?1 where accountfrom = ?2")
+    int UpdateDayLimit (BigDecimal limit, String iban);
 }
